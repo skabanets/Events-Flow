171ba333520d475b8ffdcb150f505d3d37e02b5b
Sergey Kabanets <skabanets97@gmail.com>
2024-05-14 14:58:34 +0300

    add home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,33 @@
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  const handleClickButton = () => {
+    navigate('events-board');
+  };
+
+  return (
+    <div>
+      <div className="hero min-h-screen ">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="font-nunito-semibold text-4xl text-gray-900">Welcome to Events-Flow!</h1>
+            <p className="py-6">
+              Events-Flow is your premier event registration platform, meticulously crafted for
+              individuals who prioritize their time and seek to stay informed about the latest
+              events. It offers a straightforward and convenient means to browse, register for, and
+              participate in a wide array of events. Join Events-Flow now and immerse yourself in a
+              world of captivating experiences!
+            </p>
+            <button className="btn button" onClick={handleClickButton}>
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
