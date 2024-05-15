8ba79b18c37a6a044bf4efd24ac2b298792c8629
Sergey Kabanets <skabanets97@gmail.com>
2024-05-16 01:56:16 +0300

    change getWeekdays function and styles index.css

--- src/helpers/getWeekdays.ts
+++ src/helpers/getWeekdays.ts
@@ -1,16 +1,14 @@
 export const getWeekdays = (dateArr: string[]): string[] => {
   const weekdays: string[] = [];
   const today: number = new Date().getDay();
-  const daysOfWeek: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
   dateArr.forEach(dateString => {
     const date: Date = new Date(dateString);
-    const dayOfWeek: string = daysOfWeek[date.getDay()];
 
     if (date.getDay() === today) {
       weekdays.push('Today');
     } else {
-      weekdays.push(`${date.getDate()}-${dayOfWeek}`);
+      weekdays.push(`${date.getDate()}`);
     }
   });
 
